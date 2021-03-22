4a665f78a2ede655c19ca118263202bbe2bd2e0b
Ahmed_Zaky <ahmed6mohamed6@gmail.com>
2021-03-22 05:56:18 +0200

    implement get specific plant route and method

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,5 +1,5 @@
 from re import S
-from flask import Flask, json, jsonify, request
+from flask import Flask, json, jsonify, request, abort
 from flaskr.models import setup_db, Plant
 from flask_cors import CORS
 import os
@@ -18,17 +18,32 @@ def create_app(test_config=None):
 
   @app.route("/plants", methods=['GET'])
   def get_plants():
+  
     page = request.args.get('page', 1, type=int)
     start = (page - 1) * 10
     end = start + 10
+  
     plants = Plant.query.all()
     formatted_plants = [plant.format() for plant in plants]
+  
     return jsonify({
         "success":True,
         "plants": formatted_plants[start:end],
         "total_plants": len(formatted_plants)
       })
-  
+
+  @app.route("/plants/<int:plant_id>")
+  def get_specific_plant(plant_id):
+    plant = Plant.query.filter(Plant.id == plant_id).one_or_none()
+
+    if plant is None:
+      abort(404)
+    else:
+      return jsonify({
+        'success': True,
+        'plant': plant.format()
+      })
+
   return app
 
   ###################
